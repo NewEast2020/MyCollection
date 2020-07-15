b237e5a54691948c551b945438a786f7371fcea2
nim.yuan <nwyuan2011@gmail.com>
2020-07-15 15:21:40 +0200

    Plant met Inheritance

--- plant/SpelenInheritance.java
+++ plant/SpelenInheritance.java
@@ -1,6 +1,6 @@
 package plant;
 
-public class SpelenMetPlanten {
+public class SpelenInheritance {
     public static void main(String[] args) {
         Plant fuchsia = new Fuchsia(20);
         Plant klimop = new Klimop(50);

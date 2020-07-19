623f3b956971d88af9a5c8b95f36a0add7ff5de3
nim.yuan <nwyuan2011@gmail.com>
2020-07-19 23:55:10 +0200

    Trein

---
+++ trein1/NSTrein.java
@@ -0,0 +1,18 @@
+package trein1;
+
+class NSTrein {
+    static int spoorBreedte = 130; // Dit is een class variabele ook wel static variabele genoemd
+    int locNr; // default 0 (zie H2) // Dit is een instance variabele ook wel attributes, fields, non static variabelen
+    // Wordt ook wel de state van een instantie genoemd.
+
+    static int locNrStart = 100;
+    int gegenereerdelocNr;
+
+    static NSTrein maakEenNSTrein() {
+        NSTrein nsTrein = new NSTrein();
+        locNrStart++; // locNr ophogen
+        nsTrein.gegenereerdelocNr = locNrStart; // Vul het veld van de trein met de statische (zojuist opgehoogde) waarde
+        // En hier uiteraard nog heel veel code om de Trein heel specifiek te maken.
+        return nsTrein;
+    }
+}
---
+++ trein1/SpelenMetTreinen1.java
@@ -0,0 +1,42 @@
+package trein1;
+
+public class SpelenMetTreinen1 {
+    public static void main(String[] args) {
+        System.out.println("spoorbreedte: " + NSTrein.spoorBreedte);
+        NSTrein.spoorBreedte = 120; // static variabelen kun je op de static manier (via de class) en via de instance benaderen
+        //        NSTrein.locNr = 1234978;   // instance variabelen kun je alleen via de instances benaderen en NOOIT via de class direct.
+        System.out.println("spoorbreedte: " + NSTrein.spoorBreedte);
+
+        NSTrein trein1 = new NSTrein(); // new NSTrein() daarmee roepen we de (verborgen) constructor aan
+        NSTrein trein2 = new NSTrein();
+
+        System.out.println("spoorbreedtes: " + trein1.spoorBreedte + " " + trein2.spoorBreedte);
+        trein1.spoorBreedte = 100;
+        System.out.println("spoorbreedtes: " + trein1.spoorBreedte + " " + trein2.spoorBreedte);
+        trein2.spoorBreedte = 140;
+        System.out.println("spoorbreedtes: " + trein1.spoorBreedte + " " + trein2.spoorBreedte);
+
+        System.out.println("locNrs: " + trein1.locNr + " " + trein2.locNr);
+        trein1.locNr = 12312;
+        trein2.locNr = 12314;
+        System.out.println("locNrs: " + trein1.locNr + " " + trein2.locNr);
+
+        NSTrein nsTrein3 = NSTrein.maakEenNSTrein();
+        System.out.println("locNr Gegenereerd: " + nsTrein3.gegenereerdelocNr);
+        NSTrein nsTrein4 = NSTrein.maakEenNSTrein();
+        System.out.println("locNr Gegenereerd: " + nsTrein3.gegenereerdelocNr);
+        System.out.println("locNr Gegenereerd: " + nsTrein4.gegenereerdelocNr);
+    }
+}
+/* output
+spoorbreedte: 130
+        spoorbreedte: 120
+        spoorbreedtes: 120 120
+        spoorbreedtes: 100 100
+        spoorbreedtes: 140 140
+        locNrs: 0 0
+        locNrs: 12312 12314
+        locNr Gegenereerd: 101
+        locNr Gegenereerd: 101
+        locNr Gegenereerd: 102
+ */

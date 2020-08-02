473204b2493b9ec773a6d3fa3419733b7c5e0792
nim.yuan <nwyuan2011@gmail.com>
2020-08-03 00:03:47 +0200

    Inheritance 3

--- c6x_Inheritance/Inheritance.java
+++ c6x_Inheritance/Inheritance.java
@@ -113,6 +113,7 @@ class DemoInheritanceMetFields1 {
         // (Auto)(Tafel) => compileert niet
     }
 }
+class  Auto{}
 
 class Meubelair1 extends Object {
     int aantalPoten = 7;
@@ -130,7 +131,7 @@ class Stoel1 extends Meubelair1 {
 //        vervolg Nu met field alleen bij Tafel (Child) // werkt hetzelfde als bij de static voorbeelden
 //        =========
 
-public class DemoInheritanceMetFields2 {
+class DemoInheritanceMetFields2 {
     public static void main(String[] args) {
         Meubelair2 meubelairMeubilair = new Meubelair2();
         Meubelair2 meubelairTafel = new Tafel2();
@@ -403,27 +404,27 @@ class Fanta2 extends NonAlcoholischeDrank2 {
 
 class DemoInheritancePolymorphisme4 {
     public static void main(String[] args) {
-        Drank4 whiskey = new Whiskey4();
-        Drank4 fanta = new Fanta4();
+        Drank3 whiskey = new Whiskey3();
+        Drank3 fanta = new Fanta3();
         Drank4 cola = new Cola4();
         whiskey.drinken();
         fanta.drinken();
         cola.drinken();
 
-        AlcoholischeDrank4 whiskey2 = (Whiskey4)whiskey;
+        AlcoholischeDrank3 whiskey2 = (Whiskey3)whiskey;
         whiskey2.alcoholPercentage = 40;
         whiskey2.toonAlcoholPercentage();
 
-        Drank4 bier = new Bier4();
+        Drank3 bier = new Bier3();
         bier.drinken();
         System.out.println();
         // veld alcoholpercentage is nu dubbel. DUS: kijk naar het type van links (of naar de Cast, dat heeft hetzelfde effect
-        ((AlcoholischeDrank4)bier).alcoholPercentage = 8;  // links AlcoholischeDrank, dus int veld van class AlcoholischeDrank
-        System.out.println("int veld bij Alc Drank: " + ((AlcoholischeDrank4)bier).alcoholPercentage);
-        ((Bier4)bier).alcoholPercentage = 5; // links Bier, dus int veld van class Bier
-        System.out.println("int veld bij Bier     : " + ((Bier4)bier).alcoholPercentage);
-        ((AlcoholischeDrank4)bier).toonAlcoholPercentage();  // hier treedt polymorphisme op, want links Alc.Drank, maar gedrag is overriden, dus methode van Bier uitvoeren.
-        ((Bier4)bier).toonAlcoholPercentage();
+        ((AlcoholischeDrank3)bier).alcoholPercentage = 8;  // links AlcoholischeDrank, dus int veld van class AlcoholischeDrank
+        System.out.println("int veld bij Alc Drank: " + ((AlcoholischeDrank3)bier).alcoholPercentage);
+        ((Bier3)bier).alcoholPercentage = 5; // links Bier, dus int veld van class Bier
+        System.out.println("int veld bij Bier     : " + ((Bier3)bier).alcoholPercentage);
+        ((AlcoholischeDrank3)bier).toonAlcoholPercentage();  // hier treedt polymorphisme op, want links Alc.Drank, maar gedrag is overriden, dus methode van Bier uitvoeren.
+        ((Bier3)bier).toonAlcoholPercentage();
 //        bier.toonAlcoholPercentage(); // Zo kom ik er niet bij want we verwijzen naar de instantie van bier vanuit een orv van het type Drank en deze kent die methode niet.
     }
 }

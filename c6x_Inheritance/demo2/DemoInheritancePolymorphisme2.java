f8d2483556b36766fbe1ec7886b793f4857aa967
nim.yuan <nwyuan2011@gmail.com>
2020-08-04 23:04:12 +0200

    Inheritance demo2 output

--- c6x_Inheritance/demo2/DemoInheritancePolymorphisme2.java
+++ c6x_Inheritance/demo2/DemoInheritancePolymorphisme2.java
@@ -12,3 +12,9 @@ class DemoInheritancePolymorphisme2 {
         cola.drinken();
     }
 }
+/* output
+Bier drink je met grote slokken. Klok, klok... Leeg
+De mensen die dit drinken, drinken dit met kleine slokken, maar David heeft aan 1 slok genoeg.
+Fanta2, lekker fris op voor op een warme dag, wanneer het zonnetje schijnt.
+Cola2, is het lekkerst met een ijsklontje en met prik. Zonder prik is het niet te zuipen.
+ */

f05663b62bf74d31e197cd28e50f46d50ffc76b6
nim.yuan <nwyuan2011@gmail.com>
2020-08-06 23:59:41 +0200

    Inheritance7_MetFields1

--- c6x_Inheritance/inheritance6_computer/Computer.java
+++ c6x_Inheritance/inheritance6_computer/Computer.java
@@ -1,4 +1,4 @@
-package c6x_Inheritance;
+package c6x_Inheritance.inheritance6_computer;
 
 abstract class Computer { // Wanneer je een class abstract maakt, kun je er geen instanties meer van maken.
     int geheugenGrootte;

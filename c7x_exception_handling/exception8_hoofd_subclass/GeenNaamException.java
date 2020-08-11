f92721c0f6fef4860b428a448896fff4e1bbd7df
NewEast2020 <nwyuan2011@gmail.com>
2020-08-12 00:19:27 +0200

    exception8 HoofdClass plus GeenNaamException

---
+++ c7x_exception_handling/exception8_hoofd_subclass/GeenNaamException.java
@@ -0,0 +1,4 @@
+package c7x_exception_handling.exception8_hoofd_subclass;
+
+class GeenNaamException extends Exception {
+}
---
+++ c7x_exception_handling/exception8_hoofd_subclass/HoofdClass.java
@@ -0,0 +1,17 @@
+package c7x_exception_handling.exception8_hoofd_subclass;
+
+class HoofdClass {
+    String naam;
+
+    HoofdClass() throws GeenNaamException {
+        try {
+            naam.length();
+        } catch (NullPointerException npe) {
+            throw new GeenNaamException();
+        }
+    }
+
+    HoofdClass(String naam) {
+        this.naam = naam;
+    }
+}

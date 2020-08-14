8a3390f6d4f56f689da5460db7ef13b1e2249e3a
NewEast2020 <nwyuan2011@gmail.com>
2020-08-14 23:47:40 +0200

    Datatypes003_overflow

---
+++ c2x_datatypes/Datatypes003_overflow.java
@@ -0,0 +1,52 @@
+package c2x_datatypes;
+
+public class Datatypes003_overflow {
+// Overflow : results in inaccurate representation of input
+// Underflow ; results in imprecise representation of input
+
+    public static void main(String[] args) {
+    Oef001();
+    Oef002();
+    Oef003();
+    Oef004();
+    Oef005();
+    Oef006();
+    Oef007();
+    Oef008();
+    Oef009();
+    Oef010();
+    }
+
+    static void Oef001(){
+
+    }
+    static void Oef002(){
+
+    }
+    static void Oef003(){
+
+    }
+    static void Oef004(){
+
+    }
+    static void Oef005(){
+
+    }
+    static void Oef006(){
+
+    }
+    static void Oef007(){
+
+    }
+    static void Oef008(){
+
+    }
+    static void Oef009(){
+
+    }
+    static void Oef010(){
+
+    }
+}
+
+

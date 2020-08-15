4190f5e8594c407c306e074ce08a268a1ac1206e
NewEast2020 <nwyuan2011@gmail.com>
2020-08-16 01:32:35 +0200

    References002_java_identifiers oef001 identify legal/illegal java identifiers

---
+++ c2x_datatypes/References002_java_identifiers.java
@@ -0,0 +1,34 @@
+package c2x_datatypes;
+
+public class References002_java_identifiers {
+
+    public static void main(String[] args) {
+        Oef001();
+    }
+
+    // java identifiers can be names of
+    // variables
+    // methods
+    // classes
+    // interfaces
+    // and members 2229
+    // Exam : identify valid identifiers 2237
+    // there are three rules for legal identifiers
+
+    // 1.must begin with a letter
+    // 2.must begin with a $ (dollar)
+    // 3.must begin with a _ (underscore) 22:39
+
+    // 4.can not use special characters except $ and _
+    // 5.can not use Java reserved words
+
+    static void Oef001() {
+        // Are they valid identifiers
+        int _123;            // yes
+        //int 1 inifnioteloop; // xxxx letter $ _
+        int $123$;           // yes
+        //int int;             // xxxx no java reserved keyyword
+        int True;            // yes
+        //int public@;         // xxx no use of spectial characters
+    }
+}

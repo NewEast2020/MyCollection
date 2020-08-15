ab67d2afed1f234bceb4bab78425dd60bc4aa9aa
NewEast2020 <nwyuan2011@gmail.com>
2020-08-15 23:48:46 +0200

    Datatypes003_overflow Oef002

--- c2x_datatypes/Datatypes003_overflow.java
+++ c2x_datatypes/Datatypes003_overflow.java
@@ -5,7 +5,7 @@ public class Datatypes003_overflow {
 // Underflow ; results in imprecise representation of input
 
     public static void main(String[] args) {
-        Oef001();
+//        Oef001();
         Oef002();
         Oef003();
         Oef004();
@@ -17,22 +17,31 @@ public class Datatypes003_overflow {
         Oef010();
     }
 
-    static void Oef001() {
+    static void Oef001() { // example of overflow
         int max = Integer.MAX_VALUE;
         System.out.println("max = " + max);
         System.out.println('0' + Integer.toBinaryString(max));
-        max = Integer.MAX_VALUE + 1;
+        max = Integer.MAX_VALUE + 1; // add 1 to max
         System.out.println("max = " + max);
         System.out.println(Integer.toBinaryString(max));
     }
     // max = 2147483647
-    // 01111111111111111111111111111111
-    // max = -2147483648
+    // 01111111111111111111111111111111  0 shows it is positive number
+    // max = -2147483648 // smallest number
     // 10000000000000000000000000000000
 
-    static void Oef002() {
-
+    static void Oef002() { // example of overflow
+        int min = Integer.MIN_VALUE;
+        System.out.println("min = " + min);
+        System.out.println(Integer.toBinaryString(min));
+        int too_small = min - 1;  // substract -1 to min
+        System.out.println("too_small = " + too_small);
+        System.out.println('0' + Integer.toBinaryString(too_small));
     }
+   // min = -2147483648
+   // 10000000000000000000000000000000  // 1 shows it is a negative number
+   // too_small = 2147483647 // largest number
+   // 01111111111111111111111111111111
 
     static void Oef003() {
 

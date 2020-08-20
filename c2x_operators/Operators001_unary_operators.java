66adbf8fd73e41d3b2fad8ef4fad7c9b9d36377d
NewEast2020 <nwyuan2011@gmail.com>
2020-08-20 23:51:39 +0200

    Operators001_unary_operators oef001

---
+++ c2x_operators/Operators001_unary_operators.java
@@ -0,0 +1,72 @@
+package c2x_operators;
+
+public class Operators001_unary_operators {
+
+    public static void main(String[] args) {
+        Oef001();
+        Oef002();
+        Oef003();
+        Oef004();
+        Oef005();
+        Oef006();
+        Oef007();
+        Oef008();
+        Oef009();
+        Oef010();
+    }
+
+    // computing is about information processes
+    // information is represented as data
+    // we can operate on them
+    // we call them operators
+    // understand the functions they perform
+    // and how they behave
+    // specially uin the presence of other operators
+    // operators can be categorized in many different ways (10 categories)
+    // number of operands they can take: unary, binary, ternary operators
+    // each oprerators and operand forms an expression
+    // each operand can be a subexpression, consist of other operators and their operands
+
+    // an expression can always be evaluated into a single value
+    // by evaluating all their sub expressions
+
+    static void Oef001(){
+        int a = -42;            // unary minus operator indicates sign of de literal value
+        System.out.println(+a); // unary plus operator
+        System.out.println(-a); // unary minus operator negate the value
+        System.out.println(a);  // prints the value stored in variable a
+    }
+    // -42
+    //  42
+    // -42
+
+    static void Oef002(){
+
+    }
+    static void Oef003(){
+
+    }
+    static void Oef004(){
+
+    }
+    static void Oef005(){
+
+    }
+    static void Oef006(){
+
+    }
+    static void Oef007(){
+
+    }
+    static void Oef008(){
+
+    }
+    static void Oef009(){
+
+    }
+    static void Oef010(){
+
+    }
+}
+
+

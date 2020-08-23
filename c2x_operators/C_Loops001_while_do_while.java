5be48451d8b04e4135abdb78554e0099c9b110a7
NewEast2020 <nwyuan2011@gmail.com>
2020-08-24 00:25:36 +0200

    C_Loops001_while_do_while oef2

--- c2x_operators/C_Loops001_while_do_while.java
+++ c2x_operators/C_Loops001_while_do_while.java
@@ -23,7 +23,12 @@ public class C_Loops001_while_do_while {
         }
     }
     static void Oef002(){
-
+//int count = 0;
+        do{
+            int count = 0;
+            System.out.println("Hello World!");
+            count++;
+        }while(count < 10);
     }
     static void Oef003(){
 

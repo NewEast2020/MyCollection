d3aab06a05cd5561d98453c701ec8f779095e7fe
NewEast2020 <nwyuan2011@gmail.com>
2020-08-23 23:12:52 +0200

    A_Operators003_precedence_order delete oef5 tm 10

--- c2x_operators/A_Operators003_precedence_order.java
+++ c2x_operators/A_Operators003_precedence_order.java
@@ -7,12 +7,6 @@ public class A_Operators003_precedence_order {
 //        Oef002();
 //        Oef003();
         Oef004();
-        Oef005();
-        Oef006();
-        Oef007();
-        Oef008();
-        Oef009();
-        Oef010();
     }
 
 // While multiple operators appears in the same expression
@@ -91,25 +85,6 @@ public class A_Operators003_precedence_order {
     // not all subexpressions are evaluated
     // 1/0 is not evaluated, because it is unnecesary
 
-
-    static void Oef005(){
-
-    }
-    static void Oef006(){
-
-    }
-    static void Oef007(){
-
-    }
-    static void Oef008(){
-
-    }
-    static void Oef009(){
-
-    }
-    static void Oef010(){
-
-    }
 }
 
 

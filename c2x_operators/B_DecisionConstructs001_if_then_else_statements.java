7872f7a23e805b2ecba79a0e7225fd95d1cadaba
NewEast2020 <nwyuan2011@gmail.com>
2020-08-25 20:39:13 +0200

    B_DecisionConstructs001_if_then_else_statements delete oef004 tm 010

--- c2x_operators/B_DecisionConstructs001_if_then_else_statements.java
+++ c2x_operators/B_DecisionConstructs001_if_then_else_statements.java
@@ -6,13 +6,6 @@ public class B_DecisionConstructs001_if_then_else_statements {
 //        Oef001();
 //        Oef002();
         Oef003();
-//        Oef004();
-//        Oef005();
-//        Oef006();
-//        Oef007();
-//        Oef008();
-//        Oef009();
-//        Oef010();
     }
 // decision contruct to change the flow
 // conditional execution of code blocks
@@ -65,28 +58,6 @@ public class B_DecisionConstructs001_if_then_else_statements {
     // because of if () {} else if() {}
 
     // cannot be printed, reults in a compler error
-
-    static void Oef004(){
-
-    }
-    static void Oef005(){
-
-    }
-    static void Oef006(){
-
-    }
-    static void Oef007(){
-
-    }
-    static void Oef008(){
-
-    }
-    static void Oef009(){
-
-    }
-    static void Oef010(){
-
-    }
 }
 
 

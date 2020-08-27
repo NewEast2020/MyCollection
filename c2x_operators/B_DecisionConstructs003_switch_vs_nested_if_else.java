b4cd9ef48319de15373e868a08880fd0d9e065c9
NewEast2020 <nwyuan2011@gmail.com>
2020-08-28 01:16:24 +0200

    B_DecisionConstructs003_switch_vs_nested_if_else oef001 met output

--- c2x_operators/B_DecisionConstructs003_switch_vs_nested_if_else.java
+++ c2x_operators/B_DecisionConstructs003_switch_vs_nested_if_else.java
@@ -4,22 +4,32 @@ public class B_DecisionConstructs003_switch_vs_nested_if_else {
 
     public static void main(String[] args) {
         Oef001();
-        Oef002();
-        Oef003();
-        Oef004();
-        Oef005();
-        Oef006();
-        Oef007();
-        Oef008();
-        Oef009();
-        Oef010();
+//        Oef002();
+//        Oef003();
+//        Oef004();
+//        Oef005();
+//        Oef006();
+//        Oef007();
+//        Oef008();
+//        Oef009();
+//        Oef010();
     }
+// allow us you compare a variable with a list of literal values
+// and execute code blocks selectively
+// type of variable is limited to a handfull of types
+// any primitive integer type, vb: byte short and int
+// char
+// enums
+// String values
+
 
     static void Oef001(){
         Character c = 'A';
-        switch(c) {
-            case 'A':
+        switch(c) { // switch on a character object
+            case 'A': // literal values are primitive char values
                 System.out.println("Excellent job!");
+                // no break statement
+                // subsequent will be executed
             case 'B':
                 System.out.println("Good job!");
                 break;
@@ -27,6 +37,13 @@ public class B_DecisionConstructs003_switch_vs_nested_if_else {
                 System.out.println("Decent job!");
         }
     }
+// aloud to compare Character objects to char primitives
+// due to autoboxing
+
+// output
+// Excellent job!
+// Good job!
+
     static void Oef002(){
         int days = 0, month = 6;
         switch(month){

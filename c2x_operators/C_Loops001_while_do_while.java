14ce6892ebda4f910f67018f17a4e57fa1ee278e
NewEast2020 <nwyuan2011@gmail.com>
2020-08-29 04:11:58 +0200

    C_Loops001_while_do_while oef1 plus output

--- c2x_operators/C_Loops001_while_do_while.java
+++ c2x_operators/C_Loops001_while_do_while.java
@@ -4,24 +4,45 @@ public class C_Loops001_while_do_while {
 
     public static void main(String[] args) {
         Oef001();
-        Oef002();
-        Oef003();
-        Oef004();
-        Oef005();
-        Oef006();
-        Oef007();
-        Oef008();
-        Oef009();
-        Oef010();
+//        Oef002();
+//        Oef003();
+//        Oef004();
+//        Oef005();
+//        Oef006();
+//        Oef007();
+//        Oef008();
+//        Oef009();
+//        Oef010();
     }
+    // loop are another type of decision construct
 
+    // simple while loop
     static void Oef001(){
         int count = 0;
         while(count < 10){
-            System.out.println("Hello world!");
+            System.out.println(count+1 + " " + "Hello world!");
             count++;
         }
     }
+
+    // loopvariable count: zero, continues as long as count is less than 10
+    // count is incremented at the end of each iteration
+    // there are 10 different count values: 0 to 9
+    // loop body will be executed 10 times
+    // resulting in 10 hello world
+
+    // output
+    // 1 Hello world!
+    // 2 Hello world!
+    // 3 Hello world!
+    // 4 Hello world!
+    // 5 Hello world!
+    // 6 Hello world!
+    // 7 Hello world!
+    // 8 Hello world!
+    // 9 Hello world!
+    // 10 Hello world!
+
     static void Oef002(){
 //int count = 0;
 //        do{

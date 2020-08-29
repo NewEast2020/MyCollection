d87f0b93a6c05721bb1c64ba8ffe2542e5c28acd
NewEast2020 <nwyuan2011@gmail.com>
2020-08-29 04:54:57 +0200

    C_Loops001_while_do_while oef2 plus output comment

--- c2x_operators/C_Loops001_while_do_while.java
+++ c2x_operators/C_Loops001_while_do_while.java
@@ -3,8 +3,8 @@ package c2x_operators;
 public class C_Loops001_while_do_while {
 
     public static void main(String[] args) {
-        Oef001();
-//        Oef002();
+//        Oef001();
+        Oef002();
 //        Oef003();
 //        Oef004();
 //        Oef005();
@@ -43,18 +43,35 @@ public class C_Loops001_while_do_while {
     // 9 Hello world!
     // 10 Hello world!
 
+    // do while loop is similair to while loop
+    // convert while loop in do while loop
+    // difference with while loop :
+    // do-while loop executes loop body at least once
+    // because loop condition is checked after each iteration
+
     static void Oef002(){
-//int count = 0;
-//        do{
-//            int count = 0;
-//            System.out.println("Hello World!");
-//            count++;
-//        }while(count < 10);
+/*
+        do{
+            int count = 0; // temp comment else compile error
+                           // move count variable outside of the loop
+            System.out.println(count+1 + " (2) " + "Hello World!");
+        } while(count++ < 10);
+
     }
+*/
+// var count is out of scope
+
     static void Oef003(){
 
     }
     static void Oef004(){
+        int count = 0; // temp create variable else compile error
+        do{
+            // int count = 0; // temp comment else compile error
+            // move count variable outside of the loop
+            System.out.println(count+1 + " (2) " + "Hello World!");
+            count++;
+        } while(count < 10);
 
     }
     static void Oef005(){

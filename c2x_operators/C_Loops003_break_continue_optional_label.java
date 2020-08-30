a53fdf8707a51244a79fd997b45a5ab4fa757ae7
NewEast2020 <nwyuan2011@gmail.com>
2020-08-30 21:45:45 +0200

    C_Loops003_break_continue_optional_label oef002 output

--- c2x_operators/C_Loops003_break_continue_optional_label.java
+++ c2x_operators/C_Loops003_break_continue_optional_label.java
@@ -3,8 +3,8 @@ package c2x_operators;
 public class C_Loops003_break_continue_optional_label {
 
     public static void main(String[] args) {
-        Oef001();
-//        Oef002();
+//        Oef001();
+        Oef002();
 //        Oef003();
 //        Oef004();
 //        Oef005();
@@ -50,7 +50,7 @@ public class C_Loops003_break_continue_optional_label {
         for (int row = 0; row < array.length; row++) {
             for (int col = 0; col < array[row].length; col++) {
                 if (array[row][col] == 0) {
-                    break end;
+                    break end; // jumps to first the statement after the outer loop
                 }
                 System.out.println("(1) row: " + row + " col: " + col + "  " + array[row][col]);
 //                System.out.println(array[row][col]);
@@ -67,18 +67,31 @@ public class C_Loops003_break_continue_optional_label {
 //   (1) row: 1 col: 0  4
 
     static void Oef002() {
-        int[][] array = {{1, 2, 3}, {4, 0, 5}, {6, 7, 8}};
+//        int[][] array = {{1, 2, 3}, {4, 0, 5}, {6, 7, 8}};
+        int[][] array = {{1, 2, 3 ,4 , 5}, {4, 0, 5, 6, 7}, {6, 7, 8, 9, 1 }};
 
         end:
         for (int row = 0; row < array.length; row++) {
             for (int col = 0; col < array[row].length; col++) {
                 if (array[row][col] == 0) {
-                    continue end;
+                    continue end; // terminates the iteration of the outer loop
                 }
                 System.out.println("(2) row: " + row + " col: " + col + "  " + array[row][col]);
             }
         }
     }
+// output
+//    (2) row: 0 col: 0  1
+//    (2) row: 0 col: 1  2
+//    (2) row: 0 col: 2  3
+//    (2) row: 0 col: 3  4
+//    (2) row: 0 col: 4  5
+//    (2) row: 1 col: 0  4
+//    (2) row: 2 col: 0  6
+//    (2) row: 2 col: 1  7
+//    (2) row: 2 col: 2  8
+//    (2) row: 2 col: 3  9
+//    (2) row: 2 col: 4  1
 
     static void Oef003() {
 

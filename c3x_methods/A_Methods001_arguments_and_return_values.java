96c888ba8cc1b7c367625d797e7e7875179c592c
NewEast2020 <nwyuan2011@gmail.com>
2020-09-01 21:14:22 +0200

    A_Methods001_arguments_and_return_values delete oef002 tm oef010

--- c3x_methods/A_Methods001_arguments_and_return_values.java
+++ c3x_methods/A_Methods001_arguments_and_return_values.java
@@ -3,68 +3,6 @@ package c3x_methods;
 public class A_Methods001_arguments_and_return_values {
 
     public static void main(String[] args) {
-        Oef002();
-        Oef003();
-        Oef004();
-        Oef005();
-        Oef006();
-        Oef007();
-        Oef008();
-        Oef009();
-        Oef010();
-    }
-
-    static void Oef002() {
-        A_Methods001_arguments_and_return_values m = new A_Methods001_arguments_and_return_values();
-        m.method1();
-        m.method2();
-        m.method3();
-        m.method4();
-    }
-    public void method1(){}
-
-    public void method2(){
-        return;
-    }
-
-    public Float method3(){
-        //return 12.3; // temp comment
-        return 12.3f; // temp inserted
-    }
-
-    public Float method4(){
-        return 12.3f;
-    }
-    static void Oef003() {
-
-    }
-
-    static void Oef004() {
-
-    }
-
-    static void Oef005() {
-
-    }
-
-    static void Oef006() {
-
-    }
-
-    static void Oef007() {
-
-    }
-
-    static void Oef008() {
-
-    }
-
-    static void Oef009() {
-
-    }
-
-    static void Oef010() {
-
     }
 }
 
@@ -123,8 +61,9 @@ class Main_oef002 {
 
     public Float method3_oef002(){
         // return 12.3; // commented out
-                     // compile error
-                     // java: incompatible types: double cannot be converted to java.lang.Float
+                        // it reurns a Double floating point type
+                        // compile error
+                        // java: incompatible types: double cannot be converted to java.lang.Float
 
         return 12.3f; // comment new line
     }

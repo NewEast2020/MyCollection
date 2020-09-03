30fa4c98795ae411f922fc7b77cb8e4406cc09e2
NewEast2020 <nwyuan2011@gmail.com>
2020-09-03 23:30:16 +0200

    A_Methods003_static_methods_and_fields class Main_oef301 plus commment

--- c3x_methods/A_Methods003_static_methods_and_fields.java
+++ c3x_methods/A_Methods003_static_methods_and_fields.java
@@ -67,5 +67,34 @@ public class A_Methods003_static_methods_and_fields {
 
     }
 }
+// static members belong to the class
+// only a single copy of each static member will be associated with the class object
+// significant implications how static members can be used
+// a static member is always executed in a static context
+// a static member does not have access to any instance members of the class
+// it is not possible to call an instance method in a static method in the same class
+
+class Main_oef301 {
+
+    // static print method
+    static void print(){
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        Main_oef301 m = new Main_oef301();
+        m.print(); // static method with an object reference is correct
+        print();   // static method call print directly from the static method main is correct
+        m = null;
+        m.print(); // invoking static method on a null reference is correct because
+                   // a static method is associated with its class not with any instances of a class
+                   // that why you do not need an object to invoke a static method
+    }
+    // output
+    // Hello World!
+    // Hello World!
+    // Hello World!
+}
+
 
 

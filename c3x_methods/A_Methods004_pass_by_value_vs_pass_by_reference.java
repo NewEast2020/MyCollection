0fc10581fea5a2a29db9715470153e76ac2ab222
NewEast2020 <nwyuan2011@gmail.com>
2020-09-04 00:37:19 +0200

    A_Methods004_pass_by_value_vs_pass_by_reference delete oef002 tm oef010

--- c3x_methods/A_Methods004_pass_by_value_vs_pass_by_reference.java
+++ c3x_methods/A_Methods004_pass_by_value_vs_pass_by_reference.java
@@ -3,52 +3,6 @@ package c3x_methods;
 public class A_Methods004_pass_by_value_vs_pass_by_reference {
 
     public static void main(String[] args) {
-        Oef002();
-        Oef003();
-        Oef004();
-        Oef005();
-        Oef006();
-        Oef007();
-        Oef008();
-        Oef009();
-        Oef010();
-    }
-
-
-    static void Oef002() {
-
-    }
-
-    static void Oef003() {
-
-    }
-
-    static void Oef004() {
-
-    }
-
-    static void Oef005() {
-
-    }
-
-    static void Oef006() {
-
-    }
-
-    static void Oef007() {
-
-    }
-
-    static void Oef008() {
-
-    }
-
-    static void Oef009() {
-
-    }
-
-    static void Oef010() {
-
     }
 }
 

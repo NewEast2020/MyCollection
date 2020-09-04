498bb336a195b11634041ca95cbe2df3b0722999
NewEast2020 <nwyuan2011@gmail.com>
2020-09-04 04:05:41 +0200

    B_Inheritance001_method_access_modifiers delete oef002 tm oef010

--- c3x_methods/B_Inheritance001_method_access_modifiers.java
+++ c3x_methods/B_Inheritance001_method_access_modifiers.java
@@ -6,54 +6,8 @@ import c3x_methods.package2.ClassB_oefb001;
 public class B_Inheritance001_method_access_modifiers {
 
     public static void main(String[] args) {
-        Oef002();
-        Oef003();
-        Oef004();
-        Oef005();
-        Oef006();
-        Oef007();
-        Oef008();
-        Oef009();
-        Oef010();
-    }
-
-    static void Oef002() {
-
-    }
-
-    static void Oef003() {
-
-    }
-
-    static void Oef004() {
-
-    }
-
-    static void Oef005() {
-
-    }
-
-    static void Oef006() {
-
-    }
-
-    static void Oef007() {
-
-    }
-
-    static void Oef008() {
-
-    }
-
-    static void Oef009() {
-
-    }
-
-    static void Oef010() {
-
     }
 }
-
 
 class Main_oefb001 {
     private String message_oefb001 = "hello";

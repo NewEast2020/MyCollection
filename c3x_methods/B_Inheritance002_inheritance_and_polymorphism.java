950207aaf079c6d5f7ba0e23038e878e844cc86b
NewEast2020 <nwyuan2011@gmail.com>
2020-09-04 20:06:40 +0200

    B_Inheritance002_inheritance_and_polymorphism delete oef001

--- c3x_methods/B_Inheritance002_inheritance_and_polymorphism.java
+++ c3x_methods/B_Inheritance002_inheritance_and_polymorphism.java
@@ -3,7 +3,6 @@ package c3x_methods;
 public class B_Inheritance002_inheritance_and_polymorphism {
 
     public static void main(String[] args) {
-        Oef001();
         Oef002();
         Oef003();
         Oef004();
@@ -13,17 +12,6 @@ public class B_Inheritance002_inheritance_and_polymorphism {
         Oef008();
         Oef009();
         Oef010();
-    }
-
-    static void Oef001() {
-        B_Inheritance002_inheritance_and_polymorphism m = new B_Inheritance002_inheritance_and_polymorphism();
-        B_Inheritance002_inheritance_and_polymorphism m2 = new B_Inheritance002_inheritance_and_polymorphism_oef1();
-        System.out.println(m.message_oef1());
-        System.out.println(m2.message_oef1());
-    }
-
-    Object message_oef1(){
-        return "Hello!";
     }
 
 
